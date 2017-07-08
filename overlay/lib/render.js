11d754223f513fec6dd2199e666296d2a0f9b8f5
Vasiliy Horbachenko <vasyl.horbachenko@gmail.com>
2017-07-08 21:37:54 +0300

    done it m8

--- overlay/lib/render.js
+++ overlay/lib/render.js
@@ -115,8 +115,6 @@
 
       var current_dps = window.hist.current.header.encdps
       var result = this.previous_dps != current_dps
-      console.log(current_dps + " vs " + this.previous_dps)
-      console.log(this.previous_dps == current_dps)
       this.previous_dps = current_dps
       return result
     }
